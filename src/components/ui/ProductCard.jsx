a5a18034426e6576a59ef5448b610524c4673efe
Ania Buwaj <100519699+ania221B@users.noreply.github.com>
2026-02-10 17:17:30 +0100

    refactor: redesing product card structure, layout and styling

--- src/components/ui/ProductCard.jsx
+++ src/components/ui/ProductCard.jsx
@@ -1,47 +1,55 @@
-import { ShoppingCart, Star } from 'lucide-react'
+import { Star } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
 function ProductCard ({ item }) {
-  const { name, background_image: img, rating, genres } = item
+  const { slug, name, background_image: img, rating, genres } = item
   const price = (0).toFixed(2)
   return (
-    <article className='product-item'>
-      <Link>
+    <article className='product-item'>  
+      
         <div className='product-item__img'>
           <img src={img} alt={name} width='300' height='250' />
         </div>
-        <header>
-          <h3>{name}</h3>
-          <dl className='product-item__rating'>
-            <dt className='sr-only'>Rating:</dt>
-            <dd>
-              <span className='sr-only'>Rated {rating} out of 5 stars.</span>
-              <span aria-hidden='true'>{rating} / 5</span>
-              <span className='product-item__star'>
-                <Star size={18}></Star>
-              </span>
-            </dd>
-          </dl>
-        </header>
-        <dl>
-          <dt className='sr-only'>Price:</dt>
-          <dd className='product-item__price-current'>
-            <span className='sr-only'>Current price is ${price}.</span>
-            <span aria-hidden='true'>$ {price}</span>
-          </dd>
-        </dl>
 
-        <h4 className='sr-only'>Genres:</h4>
-        <ul className='product-item__genre-list'>
-          {genres.map(genre => {
-            return <li key={genre.id}>{genre.name}</li>
-          })}
-        </ul>
+        <div className="product-item__content">     
+        <h3 className='product-item__name'>{name}</h3> 
+        
+          <div className='product-item__info'>
+            <dl>
+              <dt className='sr-only'>Price:</dt>
+              <dd className='product-item__price-current'>
+                <span className='sr-only'>Current price is ${price}.</span>
+                <span aria-hidden='true'>${price}</span>
+              </dd>
+            </dl>
+             <dl className='product-item__rating'>
+                <dt className='sr-only'>Rating:</dt>
+                <dd>
+                  <span className='sr-only'>Rated {rating} out of 5 stars.</span>
+                  <span aria-hidden='true'>{rating} / 5</span>
+                  <span className='product-item__star'>
+                    <Star size={18}></Star>
+                  </span>
+                </dd>
+          </dl>
+          <h4 className='sr-only'>Genres:</h4>
+          <ul className='product-item__genre-list'>
+            {genres.slice(0,2).map(genre => {
+              return <li key={genre.id}><Link>{genre.name}</Link></li>
+            })}
+            {genres.length > 2 && (
+              <li><Link to={`/products/${slug}`}>+{genres.length -2} more</Link></li>
+            )}
+          </ul>
+          </div>
+
+          
+        </div>
+      <Link to={`/products/${slug}`} className='btn btn--shuffle' data-button='primary'>
+        <span>See details</span>
+        <span>See details</span>
       </Link>
-      <button type='button' className='btn btn--card' data-button='primary'>
-        <ShoppingCart></ShoppingCart>
-        <span>Add to cart</span>
-      </button>
+      
     </article>
   )
 }
