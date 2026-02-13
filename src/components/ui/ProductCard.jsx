2bf617d3dc078ced8e4a5a84b69b2a92bcce843a
Ania Buwaj <100519699+ania221B@users.noreply.github.com>
2026-02-13 18:10:33 +0100

    fix: fix layout overflow on small screens

--- src/components/ui/ProductCard.jsx
+++ src/components/ui/ProductCard.jsx
@@ -5,51 +5,53 @@ function ProductCard ({ item }) {
   const { slug, name, background_image: img, rating, genres } = item
   const price = (0).toFixed(2)
   return (
-    <article className='product-item'>  
-      
-        <div className='product-item__img'>
-          <img src={img} alt={name} width='300' height='250' />
-        </div>
+    <article className='product-item'>
+      <div className='product-item__img'>
+        <img src={img} alt={name} width='300' height='250' />
+      </div>
 
-        <div className="product-item__content">     
-        <h3 className='product-item__name'>{name}</h3> 
-        
-          <div className='product-item__info'>
-            <dl>
-              <dt className='sr-only'>Price:</dt>
-              <dd className='product-item__price-current'>
-                <span className='sr-only'>Current price is ${price}.</span>
-                <span aria-hidden='true'>${price}</span>
-              </dd>
-            </dl>
-             <dl className='product-item__rating'>
-                <dt className='sr-only'>Rating:</dt>
-                <dd>
-                  <span className='sr-only'>Rated {rating} out of 5 stars.</span>
-                  <span aria-hidden='true'>{rating} / 5</span>
-                  <span className='product-item__star'>
-                    <Star size={18}></Star>
-                  </span>
-                </dd>
+      <div className='product-item__content'>
+        <h3 className='product-item__name'>{name}</h3>
+
+        <div className='product-item__info'>
+          <dl>
+            <dt className='sr-only'>Price:</dt>
+            <dd className='product-item__price-current'>
+              <span className='sr-only'>Current price is ${price}.</span>
+              <span aria-hidden='true'>${price}</span>
+            </dd>
+          </dl>
+          <dl className='product-item__rating'>
+            <dt className='sr-only'>Rating:</dt>
+            <dd>
+              <span className='sr-only'>Rated {rating} out of 5 stars.</span>
+              <span aria-hidden='true'>{rating} / 5</span>
+              <span className='product-item__star'>
+                <Star size={18}></Star>
+              </span>
+            </dd>
           </dl>
           <h4 className='sr-only'>Genres:</h4>
           <ul className='product-item__genre-list'>
-            {genres.slice(0,2).map(genre => {
-              return <li key={genre.id}><Link>{genre.name}</Link></li>
+            {genres.slice(0, 2).map(genre => {
+              return (
+                <li key={genre.id}>
+                  <Link>{genre.name}</Link>
+                </li>
+              )
             })}
             {genres.length > 2 && (
-              <li><Link to={`/products/${slug}`}>+{genres.length -2} more</Link></li>
+              <li>
+                <Link to={`/products/${slug}`}>+{genres.length - 2} more</Link>
+              </li>
             )}
           </ul>
-          </div>
-
-          
         </div>
+      </div>
       <Link to={`/products/${slug}`} className='btn' data-button='primary'>
         <span>See details</span>
         <span>See details</span>
       </Link>
-      
     </article>
   )
 }
