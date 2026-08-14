a32106e7abdd5bad878c09d3005fc6c248bea399
Ania Buwaj <100519699+ania221B@users.noreply.github.com>
2026-08-14 19:59:28 +0200

    fix: added error component for the Products page

---
+++ src/components/sections/ProductsError.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+
+function ProductsError () {
+  return (
+    <section className='section error-page error-page--not-found'>
+      <div className='container'>
+        <div className='error-page__text-content flow text-center'>
+          <h2 className='error-page__title'>Error!</h2>
+          <p className='fs-500'>Something went wrong loading games.</p>
+          <Link to='/' className='btn' data-button='primary'>
+            <span>Back Home</span>
+            <span aria-hidden='true'>Back Home</span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ProductsError
